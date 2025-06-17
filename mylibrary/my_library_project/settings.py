2a9847479656298f9f49fa4a007d5a1098d2db2b
teo-danilovic <teo@autogenai.com>
2025-06-17 17:58:15 +0100

    deployment

--- mylibrary/my_library_project/settings.py
+++ mylibrary/my_library_project/settings.py
@@ -84,21 +84,22 @@ WSGI_APPLICATION = 'mylibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if 'ON_HEROKU' in os.environ:
-    DATABASES = {
-        "default": dj_database_url.config(
-            env='DATABASE_URL',
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
-    }
-else:
+
+if DEBUG:
+    # Local development database
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'my_library_project',
         }
+    }
+else:
+    # Production database from environment variable provided by Render
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
 
 
@@ -130,9 +131,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# Always set STATIC_ROOT regardless of DEBUG mode
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Only use compressed storage in production
+if not DEBUG:
+    # Enable the WhiteNoise storage backend
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+    # Security settings for production
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
 
 
 # Default primary key field type
