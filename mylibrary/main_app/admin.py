3250d988856aa93d603776aa4775598e784acaf1
teo-danilovic <teo@autogenai.com>
2025-06-17 18:06:47 +0100

    migrations

--- mylibrary/main_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Book, ReadingSession, Tag, Photo
-
-admin.site.register(Book)
-admin.site.register(ReadingSession)
-admin.site.register(Tag)
-admin.site.register(Photo)
